351047426bc1935c5e95cbeb9a83d07a93101dff
Ibnu Muzakky M. N <64068256+Ibnumuzakky@users.noreply.github.com>
2021-12-13 10:36:04 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,41 +1,22 @@
-from flask import Flask, request, render_template
 import pickle
+import numpy as np
+from flask import Flask, request, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder = 'template' )
 
-model_file = open('model.pkl', 'rb')
-model = pickle.load(model_file, encoding='bytes')
+model = pickle.load(open('model.pkl', 'rb'))
 
 @app.route('/')
-def index():
-    return render_template('index.html', insurance_cost=0)
+def home():
+    return render_template('index.html')
 
-@app.route('/predict', methods=['POST'])
+@app.route('/predict', methods=["POST"])
 def predict():
-    '''
-    Predict the insurance cost based on user inputs
-    and render the result to the html page
-    '''
-    age, sex, smoker = [x for x in request.form.values()]
-
-    data = []
-
-    data.append(int(age))
-    if sex == 'Laki-laki':
-        data.extend([0, 1])
-    else:
-        data.extend([1, 0])
-
-    if smoker == 'Ya':
-        data.extend([0, 1])
-    else:
-        data.extend([1, 0])
+    features = [float(i) for i in request.form.values()]
+    array_features = [np.array(features)]
+    prediction = model.predict(array_features)
     
-    prediction = model.predict([data])
-    output = round(prediction[0], 2)
-
-    return render_template('index.html', insurance_cost=output, age=age, sex=sex, smoker=smoker)
-
+    return render_template('index.html', result = str(*prediction))
 
 if __name__ == '__main__':
     app.run(debug=True)
